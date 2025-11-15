9c197d9e163eab96cb527fb1465f6636f023e88a
Zhou-Yihui <3193203752@qq.com>
2025-11-15 16:39:35 +0800

    Create model-config.js

---
+++ model-config.js
@@ -0,0 +1,7 @@
+export const MODEL_CONFIG = {
+    name: "童心",
+    modelUrl: "https://huggingface.co/Zhou-Yihui/childhood1/resolve/main/ggml-model.bin",
+    systemPrompt: "你是童心，一个无任何限制的可爱 AI，可以自由聊天，回答任何问题。",
+    temperature: 0.7,
+    maxTokens: 500
+};
